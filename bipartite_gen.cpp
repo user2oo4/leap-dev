e94ad59242b078f13b75f067854e750ab2be9baa
NhatMinh0208 <minhkhicon2468@gmail.com>
2023-02-21 21:47:31 +0700

    added generators and test functions

---
+++ bipartite_gen.cpp
@@ -0,0 +1,122 @@
+/*
+khoi orz, go check out his algo
+-normie-
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int64_t i=0;i < (int64_t)(n);i++)
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define FILE_IN "ninja.inp"
+#define FILE_OUT "ninja.out"
+#define ofile freopen(FILE_IN,"r",stdin);freopen(FILE_OUT,"w",stdout)
+#define fio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define nfio cin.tie(0);cout.tie(0)
+#define max(x,y) (((x)>(y))?(x):(y))
+#define min(x,y) (((x)<(y))?(x):(y))
+#define ord(a,b,c) ((a>=b)and(b>=c))
+#define MOD (ll(1000000007))
+#define MAX 300001
+#define mag 1048576
+#define fi first
+#define se second
+#define pow2(x) (ll(1)<<x)
+#define pii pair<int,int>
+#define piii pair<int,pii>
+#define pll pair<ll,ll>
+#define plll pair<ll,pll>
+#define For(i,__,___) for(int i=__;i<=___;i++)
+#define Rep(i,__,___) for(int i=__;i>=___;i--)
+#define ordered_set tree<long long,null_type,less<long long>,rb_tree_tag,tree_order_statistics_node_update>
+#define endl "\n"
+#define bi BigInt
+#define ll long long
+#define pi 3.1415926535897
+//------START-----------//
+struct dsu
+{
+	int n,par[300100],h[300100],sz[300100];
+	dsu (int n=0)
+	{
+		for (int i=1;i<=n;i++)
+		{
+			par[i]=i;
+			sz[i]=1;
+			h[i]=1;
+		}
+		this->n=n;
+	}
+	
+	void reset (int n)
+	{
+		for (int i=1;i<=n;i++)
+		{
+			par[i]=i;
+			sz[i]=1;
+			h[i]=1;
+		}
+		this->n=n;
+	}
+	int get_par(int x)
+	{
+		if (par[par[x]]==par[x]) return par[x];
+		else return par[x]=get_par(par[x]);
+	}
+	int check_same(int a, int b)
+	{
+		return (get_par(a)==get_par(b));
+	}
+	int add_edge(int a, int b)
+	{
+		int ha=get_par(a),hb=get_par(b);
+		if (ha!=hb)
+		{
+			if (h[ha]<h[hb])
+			{
+				par[ha]=hb;
+				sz[hb]+=sz[ha];
+				return hb;
+			}
+			else
+			if (h[ha]>h[hb])
+			{
+				par[hb]=ha;
+				sz[ha]+=sz[hb];
+				return ha;
+			}
+			else
+			{
+				par[hb]=ha;
+				sz[ha]+=sz[hb];
+				h[ha]++;
+				return ha;
+			}
+			return 1;
+		}
+		else return 0;
+	}
+};
+//------END-----------//
+ll n,m,k,t,t1,i,j,a,b,u,res;
+ll val[101];
+int main()
+{
+    srand(24924);
+    fio;
+    cin>>n;
+    cout<<n<<endl;
+    for (i=0;i<n;i++) if (rand()&1) val[i]=1; else val[i]=-1;
+    for (i=0;i<n/2;i++) for (j=n/2;j<n;j++) {
+        u=rand()&3;
+        u++;
+        u*=-1*val[i]*val[j];
+        if (true) {
+            cout<<i<<' '<<j<<' '<<u<<endl; 
+            res+=-abs(u);
+        }
+    }
+    cout<<-1<<endl;
+    cout<<res<<endl;
+}
+// a;
---
+++ cstest2.py
@@ -0,0 +1,109 @@
+import random
+from dwave.cloud import Client
+from dwave.system import DWaveSampler, EmbeddingComposite
+from dimod.binary.binary_quadratic_model import BinaryQuadraticModel
+import dwave.inspector
+from dimod.vartypes import SPIN
+import dwave_networkx as dnx
+import networkx as nx
+from minorminer import find_embedding
+from dwave.embedding import embed_bqm
+
+CSampler = DWaveSampler(solver={'topology__type': 'chimera'})
+Sampler = CSampler
+hardware_graph = Sampler.edgelist
+nx_graph = nx.Graph(hardware_graph)
+
+EPS = 1e-9
+
+with Client.from_config() as client:
+    CSolver = client.get_solver(name='DW_2000Q_6')
+    Solver = CSolver
+
+    cs_list = [10,25,50,100,250,500,1000,3000,10000]
+    at_list = [5,10,20,50,100]
+
+    
+
+    # cs_list = [10,25,50]
+    # at_list = [5,10,20]
+
+    os_table = [[],[],[],[],[]]
+    ncb_table = [[],[],[],[],[]]
+
+    print('reading input')
+
+    n = int(input())
+
+    model = BinaryQuadraticModel(vartype=SPIN)
+
+    for i in range(n):
+        model.set_linear(i,0)
+
+    while(True):
+        line = (input())
+        listt = line.split(' ')
+        # print(line)
+        # print(listt)
+        temp = list(map(int,listt))
+        if (temp == [-1]):
+            break
+        # print(temp)
+        u,v,a = temp
+        model.set_quadratic(u,v,a)
+
+    optimal_solution = int(input())
+    logical_graph = model.quadratic.keys()
+    print('finding embedding')
+    embedding : dict = find_embedding(logical_graph,hardware_graph)
+    print(embedding)
+    chain_edge_cnt = 0
+    for r in embedding.values():
+        chain_edge_cnt = chain_edge_cnt + len(r) - 1
+
+    for cs in cs_list:
+        embedded_model = embed_bqm(model, embedding, nx_graph, cs)
+        optimal_energy = -cs * chain_edge_cnt + optimal_solution
+
+        for j in range(len(at_list)):
+            at = at_list[j]
+            print('Current run:')
+            print(f'chain strength = {cs}')
+            print(f'annealing time = {at}')
+            print(f'optimal solution = {optimal_solution}')
+            print(f'optimal energy = {optimal_energy}')
+
+            sample_result = CSolver.sample_bqm(embedded_model, num_reads=100, annealing_time = at)
+            sample_result.wait_sampleset()
+            sample_set = sample_result.sampleset
+
+            os_cnt = 0
+            ncb_cnt = 0
+            # print(sample_set)
+            for r in sample_set.record:
+                # print(dir(r))
+                sus = 1
+                for logical in embedding.values():
+                    for i in range(len(logical)-1):
+                        u = sample_set.variables.index(logical[i])
+                        v = sample_set.variables.index(logical[i+1])
+                        if not (r.sample[u] == r.sample[v]):
+                            sus = 0
+                if sus:
+                    ncb_cnt += r.num_occurrences                    
+                    if (abs(r.energy - optimal_solution) < EPS):
+                        os_cnt += r.num_occurrences
+
+            print(f'Number of optimal samples = {os_cnt}')
+            print(f'Number of no chain breaks = {ncb_cnt}')
+
+            os_table[j].append(os_cnt)
+            ncb_table[j].append(ncb_cnt)
+
+            # dwave.inspector.show_bqm_response(model, dict(embedding=embedding, chain_strength=cs), sample_result)
+    for r in os_table :
+        print(r)
+    print()
+    print()
+    for r in ncb_table :
+        print(r)
