5b44b8e76b9b69652e9540b34821511690f5891c
NhatMinh0208 <minhkhicon2468@gmail.com>
2023-05-21 21:49:29 +0700

    ba50, ws50

--- auto_sa.sh
+++ auto_sa.sh
@@ -1,6 +1,6 @@
-python sa_findgap2.py < instances/ws100.txt
-# python sa_findgap2.py < instances/ws50.txt
-# python sa_findgap2.py < instances/ba50.txt
+# python sa_findgap2.py < instances/ws100.txt
+python sa_findgap2.py < instances/ws50.txt
+python sa_findgap2.py < instances/ba50.txt
 # python sa_findgap2.py < instances/bi50.txt
 # python sa_findgap2.py < instances/ba20.txt
 # python sa_findgap2.py < instances/ws20.txt
