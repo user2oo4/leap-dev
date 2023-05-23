6b51cc4e5ea0f6be828deaa7a690deb9cb29afae
NhatMinh0208 <minhkhicon2468@gmail.com>
2023-05-23 15:59:04 +0700

    added the rest

--- auto_sa.sh
+++ auto_sa.sh
@@ -1,8 +1,8 @@
 # python sa_findgap2.py < instances/ws100.txt
-python sa_findgap2.py < instances/ws50.txt
-python sa_findgap2.py < instances/ba50.txt
-# python sa_findgap2.py < instances/bi50.txt
-# python sa_findgap2.py < instances/ba20.txt
-# python sa_findgap2.py < instances/ws20.txt
-# python sa_findgap2.py < instances/bi20.txt
-# python sa_findgap2.py < instances/rm25.txt
+# python sa_findgap2.py < instances/ws50.txt
+# python sa_findgap2.py < instances/ba50.txt
+python sa_findgap2.py < instances/bi50.txt
+python sa_findgap2.py < instances/ba20.txt
+python sa_findgap2.py < instances/ws20.txt
+python sa_findgap2.py < instances/bi20.txt
+python sa_findgap2.py < instances/rm25.txt
