e4ef5103619e04dca6431d023e09805b4fe9c55f
NhatMinh0208 <minhkhicon2468@gmail.com>
2023-05-28 10:04:27 +0700

    sus

--- auto_sa.sh
+++ auto_sa.sh
@@ -1,1 +1,9 @@
 python sa_findgap2.py < instances_2/ws_50_100.txt
+
+python sa_findgap2.py < instances_2/ba_50_30.txt
+python sa_findgap2.py < instances_2/ba_50_60.txt
+python sa_findgap2.py < instances_2/ba_50_100.txt
+
+python sa_findgap2.py < instances_2/ba_100_30.txt
+python sa_findgap2.py < instances_2/ba_100_60.txt
+python sa_findgap2.py < instances_2/ba_100_100.txt
