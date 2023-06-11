3d7bf25e4e0b0eb3e48324f0bccf998b04046914
NhatMinh0208 <minhkhicon2468@gmail.com>
2023-06-11 11:05:26 +0700

    Added SA cache

--- sa_findgap3.py
+++ sa_findgap3.py
@@ -3,6 +3,7 @@ import dimod
 import dwave.inspector
 import csv
 import math
+import numpy
 import dwave_networkx as dnx
 import networkx as nx
 import time
@@ -41,8 +42,37 @@ class FakeChimeraSampler(dimod.Sampler, dimod.Structured):
     def edgelist(self):
         return hw.edges.keys()
     
-    def sample(self, bqm, **parameters):
-        return SASampler.sample(bqm, **parameters)
+    cache = {}
+    
+    def sample(self, bqm, cs2, num_reads, **parameters):
+        initial_states = []
+        for i in cache.keys():
+            if (abs(cs2-i) <= 1.5):
+                initial_states.append(cache[i])
+
+        intermediate = 0
+        if (len(initial_states) == 0):
+            print('NO')
+            intermediate = SASampler.sample(bqm, num_reads=num_reads, **parameters)
+        else:
+            print('YES')
+            intermediate = SASampler.sample(bqm, initial_states=initial_states, initial_states_generator='tile', num_reads=int(num_reads/10), **parameters)
+            
+
+        best_sample = 0
+        best = 0
+        for r in intermediate.record:
+            if (r.energy < best):
+                best = r.energy
+                best_sample = r
+
+        cell = {}
+        for i in range(len(intermediate.variables)):
+            cell[intermediate.variables[i]] = best_sample.sample[i]
+        cache[round(cs2,1)] = cell
+        # print(cell)
+
+        return intermediate
 
 FCSampler = FakeChimeraSampler()
 
@@ -82,23 +112,27 @@ print(f'Optimal solution: {optimal_solution}')
 
 
 EPS = 1e-9
-cached : dict = {}
 
 def fe(S, T, **kwargs):
     return find_embedding(S, T, random_seed=123123)
 
+cache : dict[float,dict[any,float]] = {}
+
 def compute(j: float, sampler: dimod.Sampler, runs: int) -> dict[str,float]:
     print('Final run:')
     print(f'strength: {j}')
-    
+
     composite = LazyFixedEmbeddingComposite(sampler, find_embedding=fe)
-    sample_set = composite.sample(model, num_reads = runs, chain_strength = j)
+
+    sample_set = composite.sample(model, num_reads = runs, chain_strength = j, cs2 = j)
 
     s1_cnt: float = 0
     s2_cnt: float = 0
     ncb_cnt: float = 0
     avg: float = 0
     best: float = 0
+    best_sample = 0
+
     for r in sample_set.record:           
         if ( r.energy < optimal_solution * 0.995 + EPS):
             s1_cnt += r.num_occurrences
@@ -106,9 +140,47 @@ def compute(j: float, sampler: dimod.Sampler, runs: int) -> dict[str,float]:
         if ( r.energy < optimal_solution * 0.97 + EPS):
             s2_cnt += r.num_occurrences
         avg += r.energy * r.num_occurrences
-        best = min(best, r.energy)
+        if (r.energy < best):
+            best = r.energy
+            best_sample = r
         ncb_cnt += r.chain_break_fraction
-    
+
+    res: dict[str,float] = {}
+    res['p5no'] = s1_cnt / runs
+    res['3no'] = s2_cnt / runs
+    res['avg'] = avg / runs
+    res['break'] = ncb_cnt / runs
+    res['best'] = best
+    print(res)
+    return res
+
+def compute_dwave(j: float, sampler: dimod.Sampler, runs: int) -> dict[str,float]:
+    print('Final run:')
+    print(f'strength: {j}')
+
+    composite = LazyFixedEmbeddingComposite(sampler, find_embedding=fe)
+
+    sample_set = composite.sample(model, num_reads = runs, chain_strength = j)
+
+    s1_cnt: float = 0
+    s2_cnt: float = 0
+    ncb_cnt: float = 0
+    avg: float = 0
+    best: float = 0
+    best_sample = 0
+
+    for r in sample_set.record:           
+        if ( r.energy < optimal_solution * 0.995 + EPS):
+            s1_cnt += r.num_occurrences
+                                
+        if ( r.energy < optimal_solution * 0.97 + EPS):
+            s2_cnt += r.num_occurrences
+        avg += r.energy * r.num_occurrences
+        if (r.energy < best):
+            best = r.energy
+            best_sample = r
+        ncb_cnt += r.chain_break_fraction
+
     res: dict[str,float] = {}
     res['p5no'] = s1_cnt / runs
     res['3no'] = s2_cnt / runs
@@ -142,7 +214,7 @@ def get_cs_range(lb: float, ub: float) -> tuple:
     r = start_high
     for i in range(DEF_BS_RUNS):
         mid = (l+r)/2
-        if (compute(mid, FCSampler, 200)['break'] < lb):
+        if (compute(mid, FCSampler, 1000)['break'] < lb):
             r = mid
         else:
             l = mid
@@ -153,7 +225,7 @@ def get_cs_range(lb: float, ub: float) -> tuple:
     r = start_high
     for i in range(DEF_BS_RUNS):
         mid = (l+r)/2
-        if (compute(mid, FCSampler, 200)['break'] < ub):
+        if (compute(mid, FCSampler, 1000)['break'] < ub):
             r = mid
         else:
             l = mid
@@ -205,13 +277,13 @@ result.sort()
 
 print(f'Run time: {(time.time() - start_time)} seconds')
 
-f = open(f'findgap2_results_p/{iname}.csv', 'w', encoding='utf-8')
+f = open(f'findgap3_results_p/{iname}.csv', 'w', encoding='utf-8')
 f.write('abs,rel,p5no,3no,best,avg,break\n')
 sus: float = UTC(model)
 def output(j):
     print(j)
     print(j/sus)
-    u = compute(j, CSampler, 1000)
+    u = compute_dwave(j, CSampler, 1000)
     print(u)
     f.write(f'{j},{j/sus},{u["p5no"]},{u["3no"]},{u["best"]},{u["avg"]},{u["break"]}\n')
 
